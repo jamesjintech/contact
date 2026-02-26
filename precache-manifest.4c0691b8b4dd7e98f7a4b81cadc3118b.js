21d559bcfb0a4adb17d6898bbe86c51af0260d41
jamesjintech <james.jin.tech@gmail.com>
2026-02-26 13:18:54 -0800

    Updates

---
+++ precache-manifest.4c0691b8b4dd7e98f7a4b81cadc3118b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aada0b4c4eb429259837ec4d80d5be36",
+    "url": "/contact/index.html"
+  },
+  {
+    "revision": "81e78c138af4aa3936d4",
+    "url": "/contact/static/css/2.cf9fcd1c.chunk.css"
+  },
+  {
+    "revision": "ddd64672d23454b806f8",
+    "url": "/contact/static/css/main.79283a81.chunk.css"
+  },
+  {
+    "revision": "81e78c138af4aa3936d4",
+    "url": "/contact/static/js/2.38882a4f.chunk.js"
+  },
+  {
+    "revision": "d3e49d62c5e83bd760c7639e00b15fb0",
+    "url": "/contact/static/js/2.38882a4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b45dbd4cf73473334917",
+    "url": "/contact/static/js/3.e6429ce8.chunk.js"
+  },
+  {
+    "revision": "ddd64672d23454b806f8",
+    "url": "/contact/static/js/main.c60ca70b.chunk.js"
+  },
+  {
+    "revision": "376cd51c4fff88dfaa72",
+    "url": "/contact/static/js/runtime~main.4bf00b8c.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/contact/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/contact/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/contact/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/contact/static/media/slick.f97e3bbf.svg"
+  }
+]);
